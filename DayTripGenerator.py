74fccdca84c70bdb8bf355ceb7243d07bd1eb099
charliebrown1221 <chaimbrown1@yahoo.com>
2022-05-06 16:37:14 -0400

    Added full loop if user chooses n

--- DayTripGenerator.py
+++ DayTripGenerator.py
@@ -11,12 +11,13 @@ def destinationb():
     user_choice.append(choice1)
     response= input('Enter y/n: ')
     while response == ('n'):
-        print('Sorry you dont like your destination how about',(random.choice(destination)),)
+        choice1=random.choice(destination)
+        print(f'Sorry you dont like your destination how about {choice1}?')
         response= input('Enter y/n: ')
+        user_choice.append(choice1)
     else :
-        print('Awesome! Lets move on')
+        print('Awesome! Lets move on.')
 
-destinationb()
 
 def transportationb():
     choice3=random.choice(transportation)
@@ -24,25 +25,28 @@ def transportationb():
     user_choice.append(choice3)
     response= input('Enter y/n: ')
     while response == ('n'):
-        print('Sorry you dont like your transportation how about',(random.choice(transportation)),)
+        choice3=random.choice(transportation)
+        print(f'Sorry you dont like your transportation how about {choice3}?')
         response= input('Enter y/n: ')
+        user_choice.append(choice3)
     else :
-        print('Awesome! Lets move on')
+        print('Awesome! Lets move on.')
 
-transportationb()
+
 
 def entertainmentb():
     choice4=random.choice(entertainment)
-    print(f'We have selected {choice4} does this sound good?') 
-    user_choice.append(choice4)
+    print(f'We have selected {choice4} does this sound good?')
+    user_choice.append(choice4) 
     response= input('Enter y/n: ')
     while response == ('n'):
-        print('Sorry you dont like your entertainment how about',(random.choice(entertainment)),)
+        choice4=random.choice(entertainment)
+        print(f'Sorry you dont like your entertainment how about {choice4}?')
         response= input('Enter y/n: ')
+        user_choice.append(choice4)
     else :
-        print('Awesome! Lets move on')
+        print('Awesome! Lets move on.')
 
-entertainmentb()
 
 def restaurantb():
     choice2=random.choice(restaurant)
@@ -50,16 +54,28 @@ def restaurantb():
     user_choice.append(choice2)
     response= input('Enter y/n: ')
     while response == ('n'):
-        print('Sorry you dont like your restaurant how about',(random.choice(restaurant)),)
+        choice2=random.choice(restaurant)
+        print(f'Sorry you dont like your restaurant how about {choice2}?')
         response= input('Enter y/n: ')
+        user_choice.append(choice2)
     else :
-        print('Awesome! Lets move on')
+        print('Awesome! Lets move on.')
     
-restaurantb()
- 
 
 def final():
-    print('For your amazing trip, you will be arriving in',(user_choice[0]),'by',(user_choice[1]),'were you wil spend the day with',(user_choice[2]),'and you will end the the evening dining in',(user_choice[3]),)
-    print(input('do you confirm this trip enter y/n: '))
 
-final()
+    print('For your amazing trip, you will be arriving in',(user_choice[0]),'by',(user_choice[1]),'were you wil spend the day with',(user_choice[2]),'and you will end the the evening dining in',(user_choice[3]),)
+    confirm = input('Do you confirm this trip? Enter y/n: ')
+    while confirm == ('n'):
+        all()
+    else :
+        print('Enjoy your trip!')    
+
+def all():
+    destinationb()
+    transportationb()
+    entertainmentb()
+    restaurantb()
+    final()
+
+all()
